6647d5e3fa06aa4486f2a4c0850fa768f8ed7578
Xuping Feng <geophydogvon@gmail.com>
2022-05-17 15:01:54 +0800

    Update plot_spectra.py

--- Data/plot_spectra.py
+++ Data/plot_spectra.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import interpolate
 import sys
 import matplotlib as mpl
 
@@ -18,32 +17,28 @@ with open(infile, 'r') as fin:
         if i >= 3 and i <= 8:
             data.append(float(tmp[0]))
 
-PP = np.loadtxt('out.txt', dtype=complex)
-PP = abs(PP) ** 2
+p = np.loadtxt('out.txt', dtype=complex)
+p = np.abs(p)
+p /= p.max()
+p = 10 * np.log10(p**2)
+ns, nb = PP.shape
+b = np.linspace(data[4], data[5], nb) / 180 * np.pi
+s = np.linspace(data[2], data[3], ns)
 
-s = np.linspace(data[2], data[3], len(PP[:, 0]))
 if unit =='deg':
     s *= 111.19492664455
 elif unit == 'm':
     s /= 1e3
 else:
     pass
-b = np.linspace(data[4], data[5], len(PP[0])) / 180 * np.pi
-baz  = np.linspace(b[0], b[-1], 361)
-slow = np.linspace(s[0], s[-1], 201)
-inp  = interpolate.interp2d(b, s, PP, kind='cubic')
-PP   = abs(inp(baz, slow))
-PP /= PP.max()
-PP = 10 * np.log10(PP)
-col = []
+
 with open('cmap.txt', 'r') as fin:
-    for line in fin.readlines():
-        col.append(line.strip())
-cmap = mpl.colors.LinearSegmentedColormap.from_list('cmap', col, 101)
+    col = fin.readline().split()
+cmap = mpl.colors.LinearSegmentedColormap.from_list('cmap', col, 201)
 
 plt.figure(figsize=(7, 6))
 ax1 = plt.subplot(111, projection='polar')
-plt.pcolormesh(baz, slow, PP, cmap=cmap, vmin=-10, vmax=0)
+plt.pcolormesh(b, s, p, cmap=cmap, vmin=-10, vmax=0)
 cbar = plt.colorbar(shrink=0.4, pad=0.2)
 cbar.set_label(r'Beam power (dB)', fontsize=14)
 cbar.ax.tick_params(labelsize=15)
@@ -51,8 +46,6 @@ ax1.grid(color='gray', ls=(10, (5, 8)), lw=1)
 ax1.tick_params(axis='y', colors='gray', labelsize=15)
 ax1.set_theta_zero_location('N')
 ax1.set_theta_direction(-1)
-ax1.plot(b, np.ones(len(b))*s.min(), c='k', lw=2)
-ax1.plot(b, np.ones(len(b))*s.max(), c='k', lw=2)
 if unit == 'deg':
     ax1.set_xlabel('Slowness (s/deg)', fontsize=20)
 elif unit == 'm':
@@ -62,5 +55,5 @@ else:
 plt.xticks(fontsize=17)
 plt.yticks(fontsize=17)
 plt.title('%.3f-%.3f Hz'%(data[0], data[1]), fontsize=20, pad=15)
-plt.tight_layout() 
+plt.tight_layout()
 plt.show()
